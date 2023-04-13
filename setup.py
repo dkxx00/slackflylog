b089325eb44f1d2bd1b806e7d667de8e5d9b5499
Hyman <hyman_x@sina.com>
2023-04-13 10:15:50 +0800

    find

--- setup.py
+++ setup.py
@@ -6,39 +6,22 @@ import sys
 
 
 # 基於flylog, slack-client處理
-import io
-import os
-import re
 
 PY2 = sys.version_info[0] == 2
 if PY2:
     REQUIRE_PACK = ['slackclient==1.3.2']
+    __version__ = "0.0.7"
 
 else:
     REQUIRE_PACK = ['slack_sdk==3.19.5']
+    __version__ = "3.0.7"
 
 
-
-def read(*names, **kwargs):
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *names),
-        encoding=kwargs.get("encoding", "utf8")
-    ) as fp:
-        return fp.read()
-
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name='slackflylog',
-    version=find_version('slackflylog', '__init__.py'),
+    version=__version__,
     author="dkxx00",
     author_email="hymanxx6@gmail.com",
     description="基于flylog的Slack日志发送",
