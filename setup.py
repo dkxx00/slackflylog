63e7dbf227f9590cd3ed4bd6afdf369e08ce4b0c
Hyman <hyman_x@sina.com>
2023-04-13 10:05:53 +0800

    main

--- setup.py
+++ setup.py
@@ -4,25 +4,47 @@
 from setuptools import setup, find_packages
 import sys
 
-PY2 = sys.version_info[0] == 2
 
 # 基於flylog, slack-client處理
+import io
+import os
+import re
+
+PY2 = sys.version_info[0] == 2
 if PY2:
-    require_pack = ['slackclient==1.3.2']
-    pack_version = '0.0.3'
+    REQUIRE_PACK = ['slackclient==1.3.2']
+
 else:
-    require_pack = ['slack_sdk==3.19.5']
-    pack_version = '1.0.3'
+    REQUIRE_PACK = ['slack_sdk==3.19.5']
+
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='slackflylog',
-    version=pack_version,
+    version=find_version('slackflylog', 'version.py'),
     author="dkxx00",
     author_email="hymanxx6@gmail.com",
     description="基于flylog的Slack日志发送",
     license="MIT",
     packages=find_packages(),
-    install_requires =require_pack,
+    install_requires =REQUIRE_PACK,
     url="https://github.com/dkxx00/slackflylog"
 )
 
