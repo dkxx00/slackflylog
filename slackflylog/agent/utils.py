664235b50010cc91260a445ecf770befb8809514
Hyman <hyman_x@sina.com>
2023-04-14 17:40:58 +0800

    raise

--- slackflylog/agent/utils.py
+++ slackflylog/agent/utils.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-
 import sys
+import traceback
 
 PY2 = sys.version_info[0] == 2
 
@@ -11,21 +10,6 @@ else:
 
 
 def import_string(import_name, silent=False):
-    """Imports an object based on a string.  This is useful if you want to
-    use import paths as endpoints or something similar.  An import path can
-    be specified either in dotted notation (``xml.sax.saxutils.escape``)
-    or with a colon as object delimiter (``xml.sax.saxutils:escape``).
-
-    If `silent` is True the return value will be `None` if the import fails.
-
-    :param import_name: the dotted name for the object to import.
-    :param silent: if set to `True` import errors are ignored and
-                   `None` is returned instead.
-    :return: imported object
-    """
-    # force the import name to automatically convert to strings
-    # __import__ is not able to handle unicode strings in the fromlist
-    # if the module is a package
     import_name = str(import_name).replace(':', '.')
     try:
         try:
@@ -40,8 +24,6 @@ def import_string(import_name, silent=False):
         try:
             module = __import__(module_name, None, None, [obj_name])
         except ImportError:
-            # support importing modules not yet set up by the parent module
-            # (or package for that matter)
             module = import_string(module_name)
 
         try:
@@ -51,5 +33,9 @@ def import_string(import_name, silent=False):
 
     except ImportError as e:
         if not silent:
-            t, v, tb = sys.exc_info()
-            raise t, v, tb
+            # Use 'with_traceback()' method for Python 3
+            if PY2:
+                t, v, tb = sys.exc_info()
+                raise t, v, tb
+            else:
+                raise ImportError(e).with_traceback(sys.exc_info()[2])
